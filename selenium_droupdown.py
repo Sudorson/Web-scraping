e357a23aad6971e72dfdf23186f652dea4ce1727
Sudorson Kumar sarker <41531519+Sudorson@users.noreply.github.com>
2023-06-28 11:00:05 +0600

    Create selenium_droupdown.py

---
+++ selenium_droupdown.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from  selenium.webdriver.support.ui import  Select
+import  pandas as pd
+import  time
+website = 'https://www.adamchoi.co.uk/overs/detailed'
+driver = webdriver.Chrome()
+driver.get(website)
+Al_matches_button = driver.find_element(By.XPATH, '//label[@analytics-event="All matches"]')
+Al_matches_button.click()
+
+
+dropdown = Select(driver.find_element(By.ID, 'country'))
+dropdown.select_by_visible_text('Spain')
+time.sleep(3)
+matches = driver.find_elements(By.TAG_NAME, 'tr')
+
+date = []
+home_team = []
+score = []
+away_team = []
+
+for match in matches :
+    date.append(match.find_element(By.XPATH, './td[1]').text)
+    home = match.find_element(By.XPATH, './td[2]').text
+    home_team.append(home)
+    print(home)
+    score.append(match.find_element(By.XPATH, './td[3]').text)
+    away_team.append(match.find_element(By.XPATH, './td[4]').text)
+
+driver.quit()
+df = pd.DataFrame({'date': date, 'home_team': home_team, 'score': score, 'away_team': away_team})
+df.to_csv('footbaoo_data.csv', index=False)
+print(df)
+
